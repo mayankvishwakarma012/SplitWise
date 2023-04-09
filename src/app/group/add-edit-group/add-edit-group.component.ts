9e604b1bf2e92301565d30ec77c2b5b967c5967c
Mayank <vishwakarmajimk012@gmail.com>
2023-04-09 22:33:58 +0530

    Add new design of group add/edit/view

--- src/app/group/add-edit-group/add-edit-group.component.ts
+++ src/app/group/add-edit-group/add-edit-group.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-edit-group',
@@ -6,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-edit-group.component.scss']
 })
 export class AddEditGroupComponent {
+
+  // Add Edit Form group
+  addEditGroup !: FormGroup;
+
+  constructor(private fb: FormBuilder , public router : Router) {
+    this.addEditGroup = this.fb.group({
+      groupName: ['', Validators.required],
+      groupType:['home'],
+      simplifiedDebt:[false],
+      members: this.fb.array([
+        this.fb.group({
+          name: [''],
+          email: ['']
+        })
+      ])
+    });
+  }
+
+  // getter for Form array control
+  get members (){
+    return this.addEditGroup.get('members') as FormArray;
+  }
+
+//  to add member fields dynamicaly
+  addMembers(){
+    this.members?.push(
+      this.fb.group({
+        name: [''],
+        email: ['']
+      })
+    );
+  }
+
+  // delete members fields dynamicly
+  deleteMember(i : number){
+    this.members.removeAt(i);
+  }
 
 }
