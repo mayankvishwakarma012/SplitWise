0a93a1ef0bfccea31e048c11f5c653d243974168
Mayank <vishwakarmajimk012@gmail.com>
2023-04-11 16:37:08 +0530

    Add required comments in ts file

--- src/app/friends/view-friends-list/view-friends-list.component.ts
+++ src/app/friends/view-friends-list/view-friends-list.component.ts
@@ -15,23 +15,31 @@ export class ViewFriendsListComponent {
   currentUrl !: string;
   isFriendActive : boolean = false;
   friendName: any;
+
   constructor(public router : Router, public route: ActivatedRoute){
-    this.route.params.subscribe(params => {
-      this.friendName = params['name'];
-    });
 
-    this.router.events
-    .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
-    .subscribe((event: NavigationEnd) => {
-          this.currentUrl = event.url;
+    /** Using this instance of route can get the value of routing parameter.
+     * and store it in friendName variable */
 
-          for (let friend of this.friends ){
-            if(this.currentUrl == '/friends'){
-            this.isFriendActive = true;
-            break;
-          }
-        }
-      });
+        this.route.params.subscribe(params => {
+          this.friendName = params['name'];
+        });
+
+        /** This instance of router is used for get current routing path
+         * to toggle the active flag for components
+         */
+        this.router.events
+        .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+        .subscribe((event: NavigationEnd) => {
+              this.currentUrl = event.url;
+
+              for (let friend of this.friends ){
+                if(this.currentUrl == '/friends'){
+                this.isFriendActive = true;
+                break;
+              }
+            }
+          });
   }
 
 }
