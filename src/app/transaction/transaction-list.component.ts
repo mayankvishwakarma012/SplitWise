997e84b3c5a27245a8af49d0f9169c0bf207ce6c
Mayank <vishwakarmajimk012@gmail.com>
2023-04-12 11:44:09 +0530

    Transaction design complete

--- src/app/transaction/transaction-list.component.ts
+++ src/app/transaction/transaction-list.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class TransactionListComponent {
 
+  dates = ['1 April', '5 April' ,'8 April']
+  expenses = ['Ram', 'Harry', 'Raj'];
+  oweAmounts = [20,50,80]
+  owedAmounts = [50,60,24]
+
 }
